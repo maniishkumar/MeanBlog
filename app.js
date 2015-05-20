c2badd916da2bb723329058bd2acd79645944993
maniishkumar <er.manish.kr@gmail.com>
2015-05-20 13:28:21 +0530

    Blog implementation

--- app.js
+++ app.js
@@ -6,14 +6,21 @@
 var express = require('express'),
   bodyParser = require('body-parser'),
   methodOverride = require('method-override'),
-  errorHandler = require('error-handler'),
+  errorHandler = require('errorHandler'),
   morgan = require('morgan'),
   routes = require('./routes'),
   api = require('./routes/api'),
   http = require('http'),
-  path = require('path');
+  path = require('path'),
+  stylus = require('stylus'),
+  nib = require('nib');
 
 var app = module.exports = express();
+function compile(str, path) {
+  return stylus(str)
+      .set('filename', path)
+      .use(nib())
+}
 
 
 /**
@@ -30,10 +37,15 @@ app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 
 var env = process.env.NODE_ENV || 'development';
+app.use(stylus.middleware(
+    { src: __dirname + '/public'
+      , compile: compile
+    }
+))
 
 // development only
 if (env === 'development') {
-  app.use(express.errorHandler());
+  app.use(errorHandler());
 }
 
 // production only
@@ -53,6 +65,12 @@ app.get('/partials/:name', routes.partials);
 // JSON API
 app.get('/api/name', api.name);
 
+app.get('/api/posts', api.posts);
+
+app.get('/api/post/:id', api.post);
+app.post('/api/post', api.addPost);
+app.put('/api/post/:id', api.editPost);
+app.delete('/api/post/:id', api.deletePost);
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
 
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -17,11 +17,99 @@ angular.module('myApp.controllers', []).
     });
 
   }).
-  controller('MyCtrl1', function ($scope) {
-    // write Ctrl here
-
+  controller('IndexCtrl', function ($scope, $http) {
+      $http({
+        method: 'GET',
+        url: '/api/posts'
+      }).
+      success(function (data, status, headers, config) {
+        $scope.posts = data.posts;
+      }).
+      error(function (data, status, headers, config) {
+        $scope.posts = 'Error!';
+      });
   }).
-  controller('MyCtrl2', function ($scope) {
-    // write Ctrl here
+  controller('AddPostCtrl', function ($scope, $http, $location) {
+      $scope.form = {};
+      $scope.submitPost = function () {
+        $http({
+          method: 'POST',
+          url: '/api/post',
+          data: $scope.form
+        }).
+        success(function (data, status, headers, config) {
+          $location.path('/');
+        }).
+        error(function (data, status, headers, config) {
+          $scope.posts = 'Error!';
+          $location.path('/');
+        });
+      };
+  }).controller('ReadPostCtrl', function ($scope, $http, $location, $routeParams) {
+      $http({
+        method: 'GET',
+        url: '/api/post/' + $routeParams.id
+      }).
+      success(function (data, status, headers, config) {
+        $scope.post = data.post;
+      }).
+      error(function (data, status, headers, config) {
+        $scope.posts = 'Error!';
+        $location.path('/');
+      });
 
-  });
+    }).controller('EditPostCtrl', function ($scope, $http, $location, $routeParams) {
+      $scope.form = {};
+      $http({
+        method: 'GET',
+        url: '/api/post/' + $routeParams.id
+      }).
+      success(function (data, status, headers, config) {
+        $scope.form = data.post;
+      }).
+      error(function (data, status, headers, config) {
+        $scope.form = 'Error!';
+      });
+
+      $scope.editPost = function () {
+        $http({
+          method: 'PUT',
+          url: '/api/post/' + $routeParams.id,
+          data: $scope.form
+        }).
+        success(function (data, status, headers, config) {
+          $location.url('/readPost/' + $routeParams.id);
+        }).
+        error(function (data, status, headers, config) {
+          $scope.form = 'Error!';
+        });
+      };
+    }).controller('DeletePostCtrl', function ($scope, $http, $location, $routeParams) {
+      $scope.form = {};
+      $http({
+        method: 'GET',
+        url: '/api/post/' + $routeParams.id
+      }).
+      success(function (data, status, headers, config) {
+        $scope.post = data.post;
+      }).
+      error(function (data, status, headers, config) {
+        $scope.post = 'Error!';
+      });
+      $scope.deletePost = function () {
+        $http({
+          method: 'DELETE',
+          url: '/api/post/' + $routeParams.id
+        }).
+        success(function (data, status, headers, config) {
+          $location.url('/');
+        }).
+        error(function (data, status, headers, config) {
+          $scope.post = 'Error!';
+        });
+      };
+
+      $scope.home = function() {
+        $location.url('/');
+      };
+    });
